19314251490f92a6df8850ea4d9962b7765b724d
kwokronny <540476284@qq.com>
2021-02-22 01:21:50 +0800

    fix: 部署问题 fix: 主题样式问题

--- deploy/sever.sh
+++ deploy/sever.sh
@@ -2,10 +2,12 @@
 configFile="./config/config.yaml"
 if [ ! -f "$configFile" ]; then
 	echo "run install"
+	pkill -9 install;
 	chmod 774 ./install
 	nohup ./install >> log.out & sleep 1
 else
 	echo "run main"
+	pkill -9 comment-app;
 	chmod 774 ./comment-app
 	nohup ./comment-app >> log.out & sleep 1
 fi
