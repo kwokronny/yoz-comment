ca6babab17cf0f98e45ec3da759601782fa62d0e
kwokronny <540476284@qq.com>
2021-02-22 00:54:29 +0800

    fix: 部署server.sh问题

--- deploy/sever.sh
+++ deploy/sever.sh
@@ -4,10 +4,8 @@ if [ ! -f "$configFile" ]; then
 	echo "run install"
 	chmod 774 ./install
 	nohup ./install >> log.out & sleep 1
-	go run install.go
 else
 	echo "run main"
 	chmod 774 ./comment-app
 	nohup ./comment-app >> log.out & sleep 1
-	go run comment-app
 fi
